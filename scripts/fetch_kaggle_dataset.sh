f4ff8ea1d3d09624d7673d600f1ca8a1f478818b
KacperKubara <kjk1u17@soton.ac.uk>
2020-08-18 15:03:27 +0200

    ss

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,5 +2,6 @@
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 unzip  packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+ls
 mv house-prices-advanced-regression-techniques/train.csv  packages/regression_model/regression_model/datasets/
 mv house-prices-advanced-regression-techniques/test.csv  packages/regression_model/regression_model/datasets/
