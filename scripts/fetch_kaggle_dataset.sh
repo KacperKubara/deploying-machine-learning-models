f1497d53774fcaca2018c560412e358a2cb07240
KacperKubara <kjk1u17@soton.ac.uk>
2020-08-18 14:50:56 +0200

    should be okay now

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-ls packages/regression_model/regression_model/datasets/
 unzip  packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
-mv packages/regression_model/regression_model/datasets/house-prices-advanced-regression-technique/* packages/regression_model/regression_model/datasets/
+ls packages/regression_model/regression_model/datasets/
